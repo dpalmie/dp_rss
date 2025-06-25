07a67cb49f3fc5044f9de5297a3217b1cd24b9eb
Davis Palmie <72349918+dpalmie@users.noreply.github.com>
2025-06-25 12:28:11 -0400

    learning about imports

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use axum::{
     routing::get,
     Router,
 };
+use tokio::net::TcpListener;
 
 async fn hello_world() -> &'static str {
     "Hello, World!"
@@ -12,6 +13,6 @@ async fn main() {
     let app = Router::new()
         .route("/", get(hello_world));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
